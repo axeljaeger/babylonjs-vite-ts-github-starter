f7654eb1ec09b87412f1e8eef9992b7791a0cda5
Axel Jäger <axeljaeger@gmail.com>
2024-03-05 22:00:34 +0100

    Fix vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,6 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
 export default defineConfig(() => ({
     base: "https://axeljaeger.github.io/babylonjs-vite-ts-github-starter/",
-    build: {
-      outDir: 'build',
-    },
-    plugins: [react()],
   })
 );
